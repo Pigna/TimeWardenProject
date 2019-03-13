6b331f6a5d17834ee8ea8e565568eb936d1a57b7
Myron Antonissen <myron.antonissen@gmail.com>
2019-03-13 16:52:37 +0100

    interceptor

--- src/main/java/model/control/NewUserCountInterceptor.java
+++ src/main/java/model/control/NewUserCountInterceptor.java
@@ -2,14 +2,13 @@ package model.control;
 
 import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
-import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
 
-@Interceptor
 public class NewUserCountInterceptor {
 
 	@Inject
 	NewUserTracker newUserTracker;
+
 	@AroundInvoke
 	public Object aroundInvoke(InvocationContext context) throws Exception {
 		newUserTracker.track();
--- src/main/java/model/control/NewUserTracker.java
+++ src/main/java/model/control/NewUserTracker.java
@@ -1,7 +1,11 @@
 package model.control;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class NewUserTracker {
 	public void track() {
 		//TODO: track new users in db
+		Logger.getAnonymousLogger().log(Level.INFO, "New User tracked.");
 	}
 }
